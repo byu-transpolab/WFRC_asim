e7bc12e4664e3e1ea3ccd3fc6c2e7e0f9f8a44ae
cbh1996 <christianhunter96@gmail.com>
2020-06-17 10:53:30 -0500

    Add pemploy and pstudent to persons

--- example/data_configs.R
+++ example/data_configs.R
@@ -16,6 +16,17 @@ as_persons <- as_persons %>%
       age > 15 & age < 18 ~ 6,
       age > 5 & age < 16 ~ 7,
       age >= 0 & age < 6 ~ 8
+    ),
+    pstudent = case_when(
+      SCHG >= 2 & SCHG <= 14 ~ 1,
+      SCHG > 14 & SCHG <= 16 ~ 2,
+      T ~ 3
+    ),
+    pemploy = case_when(
+      WKHP >= 30 ~ 1,
+      WKHP > 0 & WKHP < 30 ~ 2,
+      age >= 16 & ESR == 3 | age >= 16 & ESR == 6 ~ 3,
+      T ~ 4
     )
   )
 write_csv(as_persons, "data/synthetic_persons2.csv")
