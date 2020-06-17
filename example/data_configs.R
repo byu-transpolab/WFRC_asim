860c9fbec14e4f518683724ae5a06b75fc6715d4
cbh1996 <christianhunter96@gmail.com>
2020-06-17 09:58:56 -0500

    Delete extra code

--- example/data_configs.R
+++ example/data_configs.R
@@ -18,48 +18,4 @@ as_persons <- as_persons %>%
       age >= 0 & age < 6 ~ 8
     )
   )
-#write_csv(as_persons, "data/synthetic_persons2.csv")
-
-
-
-
-
-people <- read_csv("output/synthetic_persons.csv")
-
-people <- people %>% 
-  mutate_at(.vars = vars(COW, WKHP, DDRS, DIS), replace_na) %>% 
-  mutate(
-    DIS_status = case_when(
-      DDRS == "1" ~ T,
-      TRUE ~ F
-    ),
-    PT_hours = case_when(
-      WKHP > 0 & WKHP < 30 ~ T,
-      TRUE ~ F
-    ),
-    FT_hours = case_when(
-      between(WKHP, 30, 99) ~ T,
-      TRUE ~ F
-    ), 
-    worker = case_when(
-      PT_hours == TRUE ~ T,
-      FT_hours == T ~ T,
-      T ~ F
-    ),
-    
-    person_type = case_when(
-      DIS_status == T & worker == T ~ "WW",
-      DIS_status == T & worker == F ~ "WN",
-      FT_hours == T ~ "FW",
-      PT_hours == T ~ "PW",
-      COW == "9" & AGEP >= 50 ~ "RT", 
-      worker == F & AGEP >= 18 ~ "NW",
-      AGEP < 18 & AGEP >= 16 ~ "SD",
-      AGEP < 16 & AGEP >= 5  ~ "SP",
-      AGEP >= 0 & AGEP < 5 ~ "PS",
-      T ~ "NONE"
-    ),
-    person_type = fct_relevel(person_type, 
-                              "FW", "PW", "NW", "RT", 
-                              "SD", "SP", "PS", "WW", "WN", "NONE")
-  )
+write_csv(as_persons, "data/synthetic_persons2.csv")
