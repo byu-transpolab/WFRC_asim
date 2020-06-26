1f46356eea9c90d6f687ade1200ea43d074c3305
cbh1996 <christianhunter96@gmail.com>
2020-06-26 15:46:51 -0600

    Update gitignore and folders

--- example/data_configs.R
+++ example/data_configs.R
@@ -10,9 +10,9 @@ as_persons <- as_persons %>%
     ptype = case_when(
       age >= 18 & SCH == 1 & WKHP >= 30 ~ 1,
       age >= 18 & SCH == 1 & WKHP > 0 & WKHP < 30 ~ 2,
-      age >= 18 & age < 65 & SCH == 1 & ESR == 3 | age >= 18 & age < 65 & SCH == 1 & ESR == 6 ~ 3,
-      age >= 65 & SCH == 1 & ESR == 3 | age >= 65 & SCH == 1 & ESR == 6 ~ 4,
-      age >= 18 & SCH == 2 | age >= 18 & SCH == 3 ~ 5,
+      age >= 18 & age < 65 & SCH == 1 & ESR == 3 | age >= 18 & age < 65 & SCH == 1 & ESR == 6 ~ 4,
+      age >= 65 & SCH == 1 & ESR == 3 | age >= 65 & SCH == 1 & ESR == 6 ~ 5,
+      age >= 18 & SCH == 2 | age >= 18 & SCH == 3 ~ 3,
       age > 15 & age < 18 ~ 6,
       age > 5 & age < 16 ~ 7,
       age >= 0 & age < 6 ~ 8
